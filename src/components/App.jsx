c364a7a7f46b2aa10a13a69980c6b6119ec6198b
Illia-Illia <Takoexerrorx@gmail.com>
2022-10-01 20:27:12 +0300

    add formik

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,14 @@
 import { Component } from 'react';
+// import { nanoid } from 'nanoid';
+import { Formik, ErrorMessage } from 'formik';
+import * as yup from 'yup';
+import { Wrap, FormContact, Label, Input, Button } from './App.styled';
+
+const schema = yup.object().shape({ name: yup.string().required() });
+
+const initialValues = {
+  name: '',
+};
 
 export class App extends Component {
   state = {
@@ -6,20 +16,38 @@ export class App extends Component {
     name: '',
   };
   render() {
+    const handleSubmit = (values, { resetForm }) => {
+      console.log(values);
+      this.setState(() => ({
+        name: values,
+      }));
+      console.log('state', this.state);
+      resetForm();
+    };
     return (
-      <div>
-        <label>
-          Name
-          <input
-            type="text"
-            name="name"
-            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-            required
-          />
-          <button type="button"> Add contact</button>
-        </label>
-      </div>
+      <Wrap>
+        <h1>Phonebook</h1>
+        <Formik
+          initialValues={initialValues}
+          onSubmit={handleSubmit}
+          validationSchema={schema}
+        >
+          <FormContact autoComplete="off">
+            <Label>
+              Name
+              <Input
+                type="text"
+                name="name"
+                pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+                title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+              ></Input>
+              <ErrorMessage component="div" name="name" />
+            </Label>
+            <Button type="submit">Add contact</Button>
+          </FormContact>
+        </Formik>
+        <ul></ul>
+      </Wrap>
     );
   }
 }
