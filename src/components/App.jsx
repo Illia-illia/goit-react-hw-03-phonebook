5dc440da5417c56b3e0f744e14fbd0a65aa445ec
Illia-Illia <Takoexerrorx@gmail.com>
2022-10-05 19:08:01 +0300

    finish step 4

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,46 +1,10 @@
 import { Component } from 'react';
 import { nanoid } from 'nanoid';
-import { Formik } from 'formik';
-import * as yup from 'yup';
-import {
-  Wrap,
-  FormContact,
-  Label,
-  Input,
-  Button,
-  ErrorText,
-} from './App.styled';
+import { Wrap } from './App.styled';
 import { Section } from './Section/Section';
 import { Filter } from './Filter/Filter';
 import { ContactsList } from './ContactsList/ContactsList';
-
-const nameValidate =
-  "^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$";
-const phoneValidate = RegExp(
-  /^((\\+[1-9]{1,4}[ \\-]*)|(\\([0-9]{1,3}\\)[ \\-]*)|([0-9]{1,4})[ \\-]*)*?[0-9]{1,4}?[ \\-]*[0-9]{1,9}?$/
-);
-
-const schema = yup.object().shape({
-  name: yup
-    .string()
-    .required()
-    .matches(
-      nameValidate,
-      "Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-    ),
-  number: yup
-    .string()
-    .required()
-    .matches(
-      phoneValidate,
-      'Phone number must be digits and can contain spaces, dashes, parentheses and can start with +'
-    ),
-});
-
-const initialValues = {
-  name: '',
-  number: '',
-};
+import { ContactForm } from './ContactForm/ContactForm';
 
 export class App extends Component {
   state = {
@@ -79,27 +43,8 @@ export class App extends Component {
     };
     return (
       <Wrap>
-        <Section title={`Phonebook`}>
-          <Formik
-            initialValues={initialValues}
-            onSubmit={handleSubmit}
-            validationSchema={schema}
-          >
-            <FormContact autoComplete="off">
-              <Label>
-                Name
-                <Input type="text" name="name"></Input>
-                <ErrorText component="div" name="name" />
-              </Label>
-              <Label>
-                Number
-                <Input type="tel" name="number"></Input>
-                <ErrorText component="div" name="number" />
-              </Label>
-              <Button type="submit">Add contact</Button>
-            </FormContact>
-          </Formik>
-        </Section>
+        <Section title={`Phonebook`}></Section>
+        <ContactForm handleSubmit={handleSubmit} />
         <Section title={`Contacts`}>
           <Filter filter={this.filter} filterContact={this.filterContact} />
           <ContactsList contacts={this.onFiltredContacts()} />
